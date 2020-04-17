77b0b796230e75bdc0eea51747b16352ec34c0b2
evan <aspaasevan@gmail.com>
2020-04-17 08:46:40 -0700

    data fetch from server and renders to screen

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,41 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+import Smurf from "./Smurf";
+
+
+
+function App() { 
+  // TODO state
+  const [data, setData] = useState([]);
+
+  // TODO useEffect 
+  useEffect(() => {
+    axios.get('http://localhost:3333/smurfs')
+    .then(response => {
+      // TODO dispatch action that adds new smurf to inital data
+      setData(response.data);
+    })
+    .catch(error => {
+      console.log(error.message)
+    })
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+      <div>Welcome to your state management version of Smurfs!</div>
+      <div>Start inside of your `src/index.js` file!</div>
+      <div>Have fun!</div>
+      <div className="smurf">
+        { 
+          data.map(smurf => {
+            return <Smurf key={smurf.id} smurf={smurf} />
+          })
+        }
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Smurf = ({smurf}) => {
+  return(
+    <>
+    <h1>Coming soon....Smurf</h1>
+    <div className="smurf-container">
+      <h2>{smurf.name}</h2>
+      <div className="attributes">
+        <p>{`${smurf.name} is ${smurf.age} smurf years old.`}</p>
+        <p>{`${smurf.name} is ${smurf.height} cm tall.`}</p>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Smurf;
